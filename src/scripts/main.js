39994eed4090d6d77fc84a27e42cd97ed610db29
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-06-19 07:57:05 +0300

    main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const thumbs = document.querySelector('#thumbs');
-const largeImg = document.querySelector('#largeImg');
 
-thumbs.addEventListener('click', (evt) => {
-  evt.preventDefault();
 
   const target = evt.target.closest('a');
 
